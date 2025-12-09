3b562e3c32f5b27086d94a67fdb92c3655094df5
Shamil-G <s.gusseynov@gmail.com>
2025-12-09 18:02:16 +0500

    Merge server

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,11 +13,7 @@ access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)
 accesslog = "logs/court-gunicorn-access.log"
 error_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s"  "%(a)s"'
 errorlog = "logs/court-gunicorn-error.log"
-<<<<<<< HEAD
-proc_name = 'PENS-GFSS'
-=======
 proc_name = 'GFSS-AKTUAR-PENSION'
->>>>>>> 2f92b950ce37c53aebb27ca598b1d0cd6764c100
 # Перезапуск после N кол-во запросов
 max_requests = 10000
 # Перезапуск, если ответа не было более 60 сек
